5b42862c634e4d993566de05f52cdc0b7072f6df
Dalla <dalux@MacBook-Air-de-Dalla.local>
2025-05-06 15:07:54 +0200

    feat: correction dufichier stats

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -6,33 +6,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.util.Assert;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 
 @SpringBootTest
 public class StatistiqueTests {
 
-    @MockBean
-    StatistiqueImpl statistiqueImpl;
+    StatistiqueImpl statistiqueImpl = new StatistiqueImpl();
 
     @Test
     void avecMockBean() throws Exception{
         Voiture voiture = mock(Voiture.class); //mock sert à créer une copie d'un objet sans dépendances
         Voiture voiture2 = mock(Voiture.class);
-        statistiqueImpl.ajouter(voiture);
-        statistiqueImpl.ajouter(voiture2);
-        when(voiture.getMarque()).thenReturn("BMW"); //on impose BMW
+
+
+
+        when(voiture.getMarque()).thenReturn("BMW"); //on impose BMW, ..
         when(voiture.getPrix()).thenReturn(1000); //si on appelle getPrix pour voiture, return = 1000
-        when(voiture2.getMarque()).thenReturn("Renault");
+
+        when(voiture2.getMarque()).thenReturn("Renault"); //when pour les mocks, explique les comportements futures
         when(voiture2.getPrix()).thenReturn(2000);
 
-        Echantillon emock = mock(Echantillon.class); //un mock de l'echantillon pour
-        when(emock.getNombreDeVoitures()).thenReturn(2);
-        when(emock.getPrixMoyen()).thenReturn(1500);
+        statistiqueImpl.ajouter(voiture);
+        statistiqueImpl.ajouter(voiture2);
 
-
-        when(statistiqueImpl.prixMoyen()).thenReturn(emock);
-        Assert.isTrue(voiture2.getMarque()=="Renault", "la marque de la deuxième voiture est Renault");
+        Echantillon e1 = statistiqueImpl.prixMoyen(); //on ne mock pas echantillon car il return le prix moyen, stats car il return le new echantillon
+        assertEquals(e1.getNombreDeVoitures(),2);
+        assertEquals(e1.getPrixMoyen(),1500);
 
 
     }
